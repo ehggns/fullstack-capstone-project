811cae93c300af7c9bac7ca017945afdfa87657b
ehggns <emhas@protonmail.com>
2025-05-15 18:30:55 -0400

    adding search routes and improving app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,40 +1,48 @@
-    require('dotenv').config();
-    const express = require('express');
-    const cors = require('cors');
-    const pinoLogger = require('./logger');
+/*jshint esversion: 8 */
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const pinoLogger = require('./logger');
 
-    const connectToDatabase = require('./models/db');
-    const {loadData} = require("./util/import-mongo/index");
-
-
-    const app = express();
-    app.use("*",cors());
-    const port = 3060;
-
-    // Connect to MongoDB; we just do this one time
-    connectToDatabase().then(() => {
-        pinoLogger.info('Connected to DB');
-    })
-        .catch((e) => console.error('Failed to connect to DB', e));
+const connectToDatabase = require('./models/db');
+const {loadData} = require("./util/import-mongo/index");
 
 
-    app.use(express.json());
+const app = express();
+app.use("*",cors());
+const port = 3060;
 
-    const giftRoutes = require('./routes/giftRoutes');
-    const pinoHttp = require('pino-http');
-    const logger = require('./logger');
+// Connect to MongoDB; we just do this one time
+connectToDatabase().then(() => {
+    pinoLogger.info('Connected to DB');
+})
+    .catch((e) => console.error('Failed to connect to DB', e));
 
-    app.use(pinoHttp({ logger }));
-    app.use('/api/gifts', giftRoutes);
-    app.use((err, req, res, next) => {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-    });
 
-    app.get("/",(req,res)=>{
-        res.send("Inside the server")
-    })
+app.use(express.json());
 
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
+// Route files
+const giftRoutes = require('./routes/giftRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const pinoHttp = require('pino-http');
+const logger = require('./logger');
+
+app.use(pinoHttp({ logger }));
+
+// Use Routes
+app.use('/api/gifts', giftRoutes);
+app.use('/api/search', searchRoutes);
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+});
+
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
